5447e70b41327f2d598e9e37c8ccec1182c0ebfc
chb80 <chb80@gmx.de>
2016-07-27 20:54:01 +0200

    Meta data adjusted

--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@
 #-------------LicenseHeader--------------
 # plugin.video.chb80_gamestar - Downloads/view videos from gamestar.de
 # Copyright (C) 2015  chb80 [chb80@gmx.de]
-# based on GamestarVideo [plugin.video.gamestar] 0.1.5 Copyright (C) 2010  Raptor 2101 [raptor2101@gmx.de]
+# based on GamestarVideo [plugin.video.gamestar] 0.1.8 Copyright (C) 2010  Raptor 2101 [raptor2101@gmx.de]
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
